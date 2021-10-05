8410a2fb5855f069a98b1c3c762cff2551e8986f
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-05 17:14:51 +0530

    ADDED: calculation of daily employee wage based on part time and full time hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -9,3 +9,25 @@
         console.log("UC1 - Employee is PRESENT");
     }
 }
+
+//UC2 Switch case
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let employeeHours = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+switch(empCheck){
+    case IS_PART_TIME: 
+            employeeHours = PART_TIME_HOURS;
+            break;
+    case IS_FULL_TIME:
+            employeeHours = FULL_TIME_HOURS;
+            break;
+    default: 
+            employeeHours = 0
+}
+let employeeWage = employeeHours * WAGE_PER_HOUR;
+console.log("UC2- Employee Wage: " + employeeWage);
